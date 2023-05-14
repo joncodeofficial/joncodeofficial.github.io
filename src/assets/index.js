87783d9f1932c252c988740e39e2e54833aa8868
jonpena <gran_200014@hotmail.com>
2023-05-14 12:54:51 -0400

    compriendo imagenes de cards

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,13 +1,13 @@
 import logo from '../../public/images/icon.png';
-import github from './github.png';
+import github from '../../public/images/github.webp';
 
 import sevenSuite from './company/sevenSuite.png';
 import norkut from './company/norkut.png';
 import nanotecks from './company/nanotecks.png';
 
-import tudolarVenezuela from './works/tudolarvenezuela.png';
-import youtubeClone from './works/youtube-clone.png';
-import oldPortfolio from './works/oldPortolio.png';
+import tudolarVenezuela from './works/tudolarvenezuela.webp';
+import youtubeClone from './works/youtube-clone.webp';
+import oldPortfolio from './works/oldPortolio.webp';
 
 export {
   logo,
